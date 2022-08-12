bb9cd6f63d5fa11dd9cf7566c6ea8db5022d384c
odunbar <odunbar@caltech.edu>
2022-08-12 16:56:14 -0700

    empty tests

---
+++ test/HyperparameterOptimizers/runtests.jl
@@ -0,0 +1,10 @@
+using Test
+using Distributions
+using StableRNGs
+using StatsBase
+using LinearAlgebra
+using Random
+
+@testset "HyperparameterOptimizers" begin
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,29 @@
+function include_test(_module)
+    println("Starting tests for $_module")
+    t = @elapsed include(joinpath(_module, "runtests.jl"))
+    println("Completed tests for $_module, $(round(Int, t)) seconds elapsed")
+    return nothing
+end
+
+@testset "EnsembleKalmanProcesses" begin
+    all_tests = isempty(ARGS) || "all" in ARGS ? true : false
+
+    function has_submodule(sm)
+        any(ARGS) do a
+            a == sm && return true
+            first(split(a, '/')) == sm && return true
+            return false
+        end
+    end
+
+    for submodule in [
+        "Samplers",
+        "Features",
+        "RandomFeatureMethod",
+        "HyperparameterOptimizers",
+    ]
+        if all_tests || has_submodule(submodule) || "RandomFeatures" in ARGS
+            include_test(submodule)
+        end
+    end
+end
