71002104dcc1ed9f077b13bf4d974ee39b355c4d
odunbar <odunbar@caltech.edu>
2022-08-16 15:50:49 -0700

    test for activations

--- src/ScalarFunctions.jl
+++ src/ScalarFunctions.jl
@@ -5,10 +5,32 @@
 using SpecialFunctions
 import Base.@kwdef
 
+export
+    ScalarFunction,
+    ScalarActivation,
+    Cosine,
+    Relu,
+    Lrelu,
+    Gelu,
+    Elu,
+    Selu,
+    Heaviside,
+    SmoothHeaviside,
+    Sawtooth,
+    Softplus,
+    Tansig,
+    Sigmoid
+
+export
+    apply_scalar_function
+
+
 abstract type ScalarFunction end
 
-struct Cosine <: ScalarActivation end
-function apply_scalar_function(sa::Cosine,r)
+apply_scalar_function(sf::ScalarFunction, r::AbstractVector) = apply_scalar_function.(Ref(sf),r) # Ref(sf) treats sf as a scalar for the broadcasting
+
+struct Cosine <: ScalarFunction end
+function apply_scalar_function(sf::Cosine, r::Real)
     return cos(r)
 end
 
@@ -17,13 +39,13 @@ end
 abstract type ScalarActivation <: ScalarFunction end
 
 struct Relu <: ScalarActivation end
-function apply_scalar_function(sa::Relu,r)
+function apply_scalar_function(sa::Relu,r::Real)
     return max(0,r)
 end
 
 struct Gelu <: ScalarActivation end
-function apply_scalar_function(sa::Gelu,r)
-    cdf = 0.5 * (1.0 + erf(x / sqrt(2.0)))
+function apply_scalar_function(sa::Gelu,r::Real)
+    cdf = 0.5 * (1.0 + erf(r / sqrt(2.0)))
     return r * cdf
 end
 
@@ -38,41 +60,41 @@ function heaviside(x,y)
     end 
 end
 
-function apply_scalar_function(sa::Heaviside,r)
+function apply_scalar_function(sa::Heaviside,r::Real)
     return heaviside(r,0.5)
 end
 
 struct Sawtooth <: ScalarActivation end
-function apply_scalar_function(sa::Sawtooth,r)
+function apply_scalar_function(sa::Sawtooth,r::Real)
     return max(0,min(2 * r, 2 - 2 * r))
 end
 
 struct Softplus <: ScalarActivation end
-function apply_scalar_function(sa::Softplus,r)
+function apply_scalar_function(sa::Softplus,r::Real)
     return log(1 + exp(-abs(r))) + max(r,0)
 end
 
 struct Tansig <: ScalarActivation end
-function apply_scalar_function(sa::Tansig,r)
+function apply_scalar_function(sa::Tansig,r::Real)
     return tanh(r)
 end
 
 struct Sigmoid <: ScalarActivation end
-function apply_scalar_function(sa::Sigmoid,r)
+function apply_scalar_function(sa::Sigmoid,r::Real)
     return 1 / (1 + exp(-r))
 end
 
 @kwdef struct Elu <: ScalarActivation
     alpha::Real = 1.0
 end
-function apply_scalar_function(sa::Elu,r) 
+function apply_scalar_function(sa::Elu,r::Real) 
     return r > 0 ? r : sa.alpha * (exp(r) - 1.0)
 end
 
 @kwdef struct Lrelu <: ScalarActivation
     alpha::Real = 0.01
 end
-function apply_scalar_function(sa::Lrelu,r)
+function apply_scalar_function(sa::Lrelu,r::Real)
     return r > 0 ? r : sa.alpha * r
 end
 
@@ -80,14 +102,14 @@ end
     alpha::Real = 1.67326
     lambda::Real = 1.0507
 end
-function apply_scalar_function(sa::Selu,r)
+function apply_scalar_function(sa::Selu,r::Real)
     return r > 0 ? sa.lambda*r : sa.lambda*sa.alpha * (exp(r) - 1.0)
 end
 
 @kwdef struct SmoothHeaviside <: ScalarActivation
     epsilon::Real = 0.01
 end
-function apply_scalar_function(sa::SmoothHeaviside,r)
+function apply_scalar_function(sa::SmoothHeaviside,r::Real)
     return 1 / 2 + (1 / pi)*atan(r / sa.epsilon)
 end
 
--- test/Features/runtests.jl
+++ test/Features/runtests.jl
@@ -5,6 +5,48 @@ using StatsBase
 using LinearAlgebra
 using Random
 
-@testset "Features" begin
+using RandomFeatures.Features
 
+@testset "Features" begin
+   @testset "ScalarFunctions" begin
+
+       af_list = [
+           Relu(),
+           Lrelu(),
+           Gelu(),
+           Elu(),
+           Selu(),
+           Heaviside(),
+           SmoothHeaviside(),
+           Sawtooth(),
+           Softplus(),
+           Tansig(),
+           Sigmoid(),
+       ]
+       # very rough tests that these are activation functions
+       for af in af_list
+           @test isa(af,ScalarActivation)
+
+           x_test_neg = collect(-1:0.1:-0.1)
+           x_test_pos = collect(0:0.1:1)
+           println("Testing", af)
+           @test all(apply_scalar_function(af, x_test_neg) .<= log(2)) # small for negative x
+           
+           if !isa(af,Sawtooth)
+               @test all(apply_scalar_function(af, x_test_pos[2:end]) - apply_scalar_function(af, x_test_pos[1:end-1]) .>= 0) # monotone increasing for positive x
+           else
+               x_test_0_0pt5 = collect(0:0.1:0.5)
+               x_test_0pt5_1 = collect(0.5:0.1:1)
+               @test all(apply_scalar_function(af, x_test_0_0pt5[2:end]) - apply_scalar_function(af, x_test_0_0pt5[1:end-1]) .>= 0) 
+               @test all(apply_scalar_function(af, x_test_0pt5_1[2:end]) - apply_scalar_function(af, x_test_0pt5_1[1:end-1]) .<= 0) 
+           end
+       end
+       
+        
+
+    end
+    
+
+
+    
 end
